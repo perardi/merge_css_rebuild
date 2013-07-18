344f7d6751560f381801c0bd6bbe04bde49eb19a
Chris Perardi <chris.perardi@merge.com>
2013-07-18 15:07:03 -0500

    Rebuild, FTP.

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+	'ftp-deploy': {
+	  build: {
+	    auth: {
+	      host: 'ftp.ascedia.com',
+	      port: 21,
+	      authKey: 'key1'
+	    },
+	    src: 'media/js',
+	    dest: '/merge.com/Merge2012/media/js',
+	    exclusions: ['media/**/.DS_Store', 'media/**/Thumbs.db', 'dist/tmp']
+	  }
+	},
+	
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+ 
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-ftp-deploy');
+
+  // Default task.
+  grunt.registerTask('default', ['ftp-deploy']);
+
+};
