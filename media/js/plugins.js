5fcbde28e10b12c5bab359bde018281d675c9820
Chris Perardi <chris.perardi@merge.com>
2013-07-26 14:52:00 -0500

    Added tracking to master JS.

--- media/js/plugins.js
+++ media/js/plugins.js
@@ -11,6 +11,41 @@ if (!(window.console && console.log)) {
 	}());
 }
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25608379-1']);
+  _gaq.push(['_setDomainName', 'merge.com']);
+  _gaq.push(['_setAllowHash', false]);
+  _gaq.push(['_setLocalRemoteServerMode']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+(function() {
+  var didInit = false;
+  function initMunchkin() {
+    if(didInit === false) {
+      didInit = true;
+      Munchkin.init('951-OFL-421');
+    }
+  }
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = document.location.protocol + '//munchkin.marketo.net/munchkin.js';
+  s.onreadystatechange = function() {
+    if (this.readyState == 'complete' || this.readyState == 'loaded') {
+      initMunchkin();
+    }
+  };
+  s.onload = initMunchkin;
+  document.getElementsByTagName('head')[0].appendChild(s);
+})();
+
 // This detects a touch (read: mobile) device and disables click functions on top-level nav links. 
 var touch_detect = {  handler: function(event){    
 
